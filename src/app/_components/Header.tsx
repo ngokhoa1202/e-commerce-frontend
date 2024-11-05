17550f5174e3f210d8edd6ac4767f64e160eef6c
ngokhoa1202 <ngokhoa1202@gmail.com>
2024-11-05 18:16:35 +0700

    Fix nav bar and create new common components

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -9,8 +9,8 @@ import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 const navigation = [
   { name: 'Home', href: '#' },
   { name: 'Classes', href: '#' },
-  { name: 'Pric', href: '#' },
-  { name: 'Company', href: '#' },
+  { name: 'Pricing', href: '#' },
+  { name: 'About us', href: '#' },
 ]
 
 export default function Header(): JSX.Element {
@@ -24,28 +24,33 @@ export default function Header(): JSX.Element {
           <a href="#" className="-m-1.5 p-5">
             <span className="sr-only"></span>
             <Image 
-              alt=""
+              alt="ZetStudy brand logo"
               src="/Logo.png"
-              className="h-12 w-auto rounded"
+              className="h-12 w-auto rounded justify-self-center hover:shadow-small"
               width={48}
               height={48}
               loading="lazy"
             />
+            <span className="brand-name text-">ZetStudy</span>
           </a>
         </div>
         <div className="flex lg:hidden">
           <button
             type="button"
             onClick={() => setMobileMenuOpen(true)}
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-800"
           >
             <span className="sr-only">Open main menu</span>
             <Bars3Icon aria-hidden="true" className="h-6 w-6" />
           </button>
         </div>
-        <div className="hidden lg:flex lg:gap-x-12">
+        <div className="lg:flex lg:gap-x-12">
           {navigation.map((item) => (
-            <a key={item.name} href={item.href} className="text-sm/6 font-semibold text-gray-900">
+            <a key={item.name} href={item.href} 
+            className="text-lg font-semibold text-gray-700 px-5 py-2 rounded-md
+              hover:bg-blue-600 hover:text-gray-200 active:bg-blue-700 active:text-gray-100
+              hover:shadow-lg active:shadow-lg
+              ">
               {item.name}
             </a>
           ))}
