803be81ed8c9d9e7e5824ec61cfe0a993c52327d
ngokhoa1202 <ngokhoa1202@gmail.com>
2024-12-04 19:07:25 +0700

    Fix classes page: mainly fix class card

--- src/app/classes/page.tsx
+++ src/app/classes/page.tsx
@@ -19,12 +19,111 @@ const sampleCourses = [
       classLink: "/course-details/advanced-js",
       curriculum: [
         { week: 1, description: "Async JavaScript" },
-        { week: 2, description: "Functional Programming" },
+        { week: 2, description: "Functional style" },
         { week: 3, description: "ES6+ Features" },
       ],
     },
     {
       id: "3",
+      title: "UI/UX Design Fundamentals",
+      description: "Master the basics of UI/UX design, focusing on user-centered design.",
+      classLink: "/course-details/ui-ux",
+      curriculum: [
+        { week: 1, description: "Understanding Users" },
+        { week: 2, description: "Design Principles" },
+        { week: 3, description: "Prototyping and Testing" },
+      ],
+    },
+    {
+      id: "4",
+      title: "UI/UX Design Fundamentals",
+      description: "Master the basics of UI/UX design, focusing on user-centered design.",
+      classLink: "/course-details/ui-ux",
+      curriculum: [
+        { week: 1, description: "Understanding Users" },
+        { week: 2, description: "Design Principles" },
+        { week: 3, description: "Prototyping and Testing" },
+      ],
+    },
+    {
+      id: "5",
+      title: "UI/UX Design Fundamentals",
+      description: "Master the basics of UI/UX design, focusing on user-centered design.",
+      classLink: "/course-details/ui-ux",
+      curriculum: [
+        { week: 1, description: "Understanding Users" },
+        { week: 2, description: "Design Principles" },
+        { week: 3, description: "Prototyping and Testing" },
+      ],
+    },
+    {
+      id: "6",
+      title: "UI/UX Design Fundamentals",
+      description: "Master the basics of UI/UX design, focusing on user-centered design.",
+      classLink: "/course-details/ui-ux",
+      curriculum: [
+        { week: 1, description: "Understanding Users" },
+        { week: 2, description: "Design Principles" },
+        { week: 3, description: "Prototyping and Testing" },
+      ],
+    },
+    {
+      id: "7",
+      title: "UI/UX Design Fundamentals",
+      description: "Master the basics of UI/UX design, focusing on user-centered design.",
+      classLink: "/course-details/ui-ux",
+      curriculum: [
+        { week: 1, description: "Understanding Users" },
+        { week: 2, description: "Design Principles" },
+        { week: 3, description: "Prototyping and Testing" },
+      ],
+    },
+    {
+      id: "8",
+      title: "UI/UX Design Fundamentals",
+      description: "Master the basics of UI/UX design, focusing on user-centered design.",
+      classLink: "/course-details/ui-ux",
+      curriculum: [
+        { week: 1, description: "Understanding Users" },
+        { week: 2, description: "Design Principles" },
+        { week: 3, description: "Prototyping and Testing" },
+      ],
+    },
+    {
+      id: "9",
+      title: "UI/UX Design Fundamentals",
+      description: "Master the basics of UI/UX design, focusing on user-centered design.",
+      classLink: "/course-details/ui-ux",
+      curriculum: [
+        { week: 1, description: "Understanding Users" },
+        { week: 2, description: "Design Principles" },
+        { week: 3, description: "Prototyping and Testing" },
+      ],
+    },
+    {
+      id: "10",
+      title: "UI/UX Design Fundamentals",
+      description: "Master the basics of UI/UX design, focusing on user-centered design.",
+      classLink: "/course-details/ui-ux",
+      curriculum: [
+        { week: 1, description: "Understanding Users" },
+        { week: 2, description: "Design Principles" },
+        { week: 3, description: "Prototyping and Testing" },
+      ],
+    },
+    {
+      id: "11",
+      title: "UI/UX Design Fundamentals",
+      description: "Master the basics of UI/UX design, focusing on user-centered design.",
+      classLink: "/course-details/ui-ux",
+      curriculum: [
+        { week: 1, description: "Understanding Users" },
+        { week: 2, description: "Design Principles" },
+        { week: 3, description: "Prototyping and Testing" },
+      ],
+    },
+    {
+      id: "12",
       title: "UI/UX Design Fundamentals",
       description: "Master the basics of UI/UX design, focusing on user-centered design.",
       classLink: "/course-details/ui-ux",
@@ -40,9 +139,15 @@ export default async function Page() {
   const courses = await fetch("https://tienclay.me/ecommerce/courses");
   console.log(courses);
   return (
-    <section className="container mx-auto p-4">
+    <section className="container px-8">
       <h1 className="font-bold text-4xl text-center mt-12">Our classes</h1>
-      <ClassList classes={sampleCourses} />
+      <div className="container grid grid-cols-12 gap-x-0 mt-10">
+        <div className="col-span-2">
+          Filter
+        </div>
+        <ClassList classes={sampleCourses} />
+      </div>
+      
     </section>
   );
 }
